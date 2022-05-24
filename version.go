7bb491584949d1d25f871ec1dd4cc38e8d56af44
Erik Agsjö <erik.agsjo@gmail.com>
2022-05-24 18:55:46 +0200

    Test tagging

--- version.go
+++ version.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"runtime/debug"
+)
+
 // Tag is set to the current git tag, or empty for dev version
 // ??? Update this when / if this makes it into a release: https://github.com/golang/go/issues/49168
 var Tag = "v0.6.0"
+
+func init() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		fmt.Printf("Build info: %v\n", info)
+	}
+}
